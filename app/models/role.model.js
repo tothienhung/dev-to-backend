c57e423d98872bc99ddde0729718bf6d676c2a4e
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-10-07 14:37:33 +0700

    return

--- app/models/role.model.js
+++ app/models/role.model.js
@@ -2,17 +2,11 @@ module.exports = (sequelize, Sequelize) => {
   const Role = sequelize.define("roles", {
     id: {
       type: Sequelize.INTEGER,
-      autoIncrement: true, // Đảm bảo id tự động tăng
       primaryKey: true
     },
     name: {
-      type: Sequelize.STRING,
-      unique: true, // Đảm bảo tên là duy nhất
-      allowNull: false // Không cho phép giá trị null
+      type: Sequelize.STRING
     }
-  }, {
-    timestamps: true, // Tự động thêm createdAt và updatedAt
-    tableName: 'roles' // Tên bảng trong cơ sở dữ liệu
   });
 
   return Role;
